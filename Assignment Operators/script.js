31155ba8151f11309b3a4b127abd71a1a08db5e8
parathinker <100070252+dulhara19@users.noreply.github.com>
2022-04-23 16:44:10 +0530

    Assignement Operators

---
+++ Assignment Operators/script.js
@@ -0,0 +1,52 @@
+//-----------------------------------------
+//--------Assignment operators-------------
+//-----------------------------------------
+
+//---Equal---
+
+var myName = 'dulhara';
+console.log(myName);
+
+//---Addition with equal---
+
+var age = 20;
+age += 10; // age = age + 10
+console.log(age);
+
+//============================================
+/* var newAge = 20
+ newAge = newAge + 10 
+ console.log(newAge) 
+ 
+{this is the result from above addition with equal in classical way}*/
+
+//---------------------------------------------
+
+/* var newage=30
+var result
+result = newage + 10
+console.log(result) 
+
+{exactly this is the same result in a different way which i have learned from arithmetic operators}*/
+
+//============================================
+
+//---subtraction with equal---
+age -= 10;
+console.log(age);
+
+//---multiplication with equal 
+age *= 10;
+console.log(age);
+
+//---division with equal---
+age /= 4;
+console.log(age);
+
+//---modules with equal---
+age %= 48;
+console.log(age);
+
+//--exponentiation with equal--
+age **= 3;
+console.log(age);
