ab01ce2aa58635a84880b00882483e847ec9c3a3
dulhara <wkldulhara@gmail.com>
2025-02-01 02:15:00 +0530

    added new lines for array destruction

--- function-react-project/array_destructure.js
+++ function-react-project/array_destructure.js
@@ -14,3 +14,11 @@ const [first,second,...rest_argument]=arr;
 console.log(first);
 console.log(second);
 console.log(rest_argument);
+
+// now lets create object
+const obj1 = {one:10,two:20,three:30,four:40}
+const {one:x,two,...rest_obj}= obj1;
+
+console.log(x);
+console.log(two);
+console.log(rest_obj);
