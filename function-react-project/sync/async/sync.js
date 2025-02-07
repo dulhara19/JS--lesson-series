eaf22900030d98d26e42621e8aee16da6dfe877f
dulhara <wkldulhara@gmail.com>
2025-02-07 12:51:50 +0530

    added sync.js

---
+++ function-react-project/sync/async/sync.js
@@ -0,0 +1,13 @@
+// if i use sync that means one after one but when it comes to async processes are working simultaniousely 
+
+function onetofive(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+
+}
+    console.log(6);
+    console.log(7);
+    console.log(8); 
