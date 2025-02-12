ab4ab3863fd076ed0b14cb5c64a86ad396e92789
dulhara <wkldulhara@gmail.com>
2025-02-12 14:12:39 +0530

    added prettier and few other files as well as new components

--- function-react-project/pure-react-app/src/App.js
+++ function-react-project/pure-react-app/src/App.js
@@ -1,24 +1,30 @@
-const Student = ()=> {
-    return React.createElement(
-        "div",
-        { id: "myDiv", className: "divcls"},
-        [React.createElement("h5",{}," nimal "),
-        React.createElement("h2",{}," nisal "),
-        React.createElement("h3",{}," kamal ")
-        ]
-    )
-}
+const Student = () => {
+  return React.createElement("div", { id: "myDiv", className: "divcls" }, [
+    React.createElement("h5", {}, " nimal "),
+    React.createElement("h2", {}, " nisal "),
+    React.createElement("h3", {}, " kamal "),
+  ]);
+};
+const StudentCrad = () => {
+  return React.createElement(
+    "div",
+    { className: "imgdiv" },
 
+    React.createElement("img", {
+      src: "./img/newdp.jpeg",
+      className: "img",
+    }),
+    React.createElement(Student),
+  );
+};
 
-
-const App = ()=>{
-    return React.createElement(
-       "div",
-       {className :"containerdiv"},
-       ["this is react container",
-        React.createElement(Student)]
-    )
-}
+const App = () => {
+  return React.createElement("div", { className: "containerdiv" }, [
+    "this is react container",
+    //React.createElement(Student),
+    React.createElement(StudentCrad),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
